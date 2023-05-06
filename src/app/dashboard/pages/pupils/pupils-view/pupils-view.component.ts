f91373366baad72d1b228cba35374823a9074428
Athirapaul1993 <paula4@roehampton.ac.uk>
2023-05-06 07:20:59 +0100

    changes on discussion forum

--- src/app/dashboard/pages/pupils/pupils-view/pupils-view.component.ts
+++ src/app/dashboard/pages/pupils/pupils-view/pupils-view.component.ts
@@ -3,66 +3,67 @@ import { BackendService } from '../../../backend.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { AuthService } from 'src/app/auth/auth.service';
 @Component({
   selector: 'app-pupils-view',
   templateUrl: './pupils-view.component.html',
-  styleUrls: ['./pupils-view.component.scss']
+  styleUrls: ['./pupils-view.component.scss'],
 })
 export class PupilsViewComponent {
-  item: any
-  remark: any
+  item: any;
+  remark: any;
 
-  commentForm!: FormGroup
-  comments: any
-  showContent:boolean = true
+  commentForm!: FormGroup;
+  comments: any;
+  showContent: boolean = true;
 
-  constructor(private fb: FormBuilder, public api: BackendService, private router: Router) {
-
+  constructor(
+    private fb: FormBuilder,
+    public api: BackendService,
+    private router: Router,
+    private auth: AuthService
+  ) {
     this.commentForm = this.fb.group({
-
       text: ['', Validators.required],
       postId: ['', Validators.required],
-      userId: ['', Validators.required]
-    })
+      userId: ['', Validators.required],
+    });
   }
 
   ngOnInit(): void {
-    let id = localStorage.getItem('pupil_id')
+    let id = localStorage.getItem('pupil_id');
 
     this.getIndividualData(id);
     this.getRecentRemark(id);
   }
-
-
+  isAuthorized() {
+    return this.auth.isAdmin();
+  }
 
   getRecentRemark(id: any) {
     this.api.getRemarks(id).subscribe((res: any) => {
-      this.remark = res.data
-      this.comments = res.comments
-    })
+      this.remark = res.data;
+      this.comments = res.comments;
+    });
   }
 
   //single info
   getIndividualData(id: any) {
-
     this.api.getOneItems(id).subscribe((res: any) => {
-      this.item = res.data[0]
+      this.item = res.data[0];
     });
   }
 
-
   commentSubmit() {
     this.commentForm.value.postId = this.remark._id;
-    this.commentForm.value.userId = localStorage.getItem('pupil_id')
+    this.commentForm.value.userId = localStorage.getItem('pupil_id');
 
     this.api.postComment(this.commentForm.value).subscribe((res: any) => {
-      this.comments = res.comments
-      this.commentForm.reset()
-
-    })
-
+      this.comments = res.comments;
+      this.commentForm.reset();
+    });
   }
- 
+
   editItem() {
     this.router.navigate(['/dashboard/edit-item']);
   }
